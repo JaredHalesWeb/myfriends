625857d70977fac40b0fafa0242f59f86ceff10c
jared hales <jared.hales2838@STU.MTEC.EDU>
2025-01-07 10:21:03 -0700

    friends :3

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
-
+const friends = []
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.get('/getCurrentDate', (req, res) => {
-
     const date = new Date()
     res.send({'currentDate':date});
 });
+app.post('/addFriend', (req, res) => {
+    console.log(`adding a friend ${req.body.name}`)
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const age = req.body.age;
+    const friend = {
+        name: name, 
+        email: email,
+        phone: phone,
+        age: age
+    };
+    friends.push(friend);
+    res.json({'friends':friends})
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
---
+++ routes/friend.js
@@ -0,0 +1,1 @@
+import {Router, express} from 'express'
