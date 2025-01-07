b537ce7fd4d5c94705a47b73e26400ea768cb8f7
jared hales <jared.hales2838@STU.MTEC.EDU>
2025-01-07 08:19:39 -0700

    init git

---
+++ app.mjs
@@ -0,0 +1,16 @@
+//const express = require('express');
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.get('/getCurrentDate', (req, res) => {
+
+    const date = new Date()
+    res.send({'currentDate':date});
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
